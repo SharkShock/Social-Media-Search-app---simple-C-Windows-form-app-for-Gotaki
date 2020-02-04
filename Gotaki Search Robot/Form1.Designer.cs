bb4e4e9a7d909c50507d38dc30d99a7bc58a2629
Vladimir Nikolic <vladimirnikolic24@gmail.com>
2020-02-04 21:57:55 +0100

    commit for cool search app

---
+++ Gotaki Search Robot/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Gotaki_Search_Robot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Indigo;
+            this.button1.ForeColor = System.Drawing.Color.AliceBlue;
+            this.button1.Location = new System.Drawing.Point(12, 80);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(664, 82);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Place Name and Run";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 52);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(664, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.ForeColor = System.Drawing.Color.Lavender;
+            this.label2.Location = new System.Drawing.Point(9, 356);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(304, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Search for 16+ different Social Media Websites";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.ForeColor = System.Drawing.Color.Plum;
+            this.label3.Location = new System.Drawing.Point(539, 356);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(137, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "PLACE FINDER APP";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.BackgroundImage = global::Gotaki_Search_Robot.Properties.Resources.pastilla;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(688, 382);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.ForeColor = System.Drawing.Color.Indigo;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "GOTAKI PLACE FINDER";
+            this.TransparencyKey = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Gotaki Search Robot/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gotaki_Search_Robot
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.button1.Click += new EventHandler(button1_Click);
+            this.AcceptButton = button1;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Process.Start("chrome.exe", "https://www.google.rs/search?q=" + textBox1.Text.Replace(' ', '+'));
+            Process.Start("chrome.exe", "https://www.google.rs/search?q=" + "+facebook+" + textBox1.Text.Replace(' ', '+') );
+            Process.Start("chrome.exe", "https://www.google.rs/search?q=" + "+instagram+" + textBox1.Text.Replace(' ', '+'));
+            Process.Start("chrome.exe", "https://www.google.rs/search?q=" + "+twitter+" + textBox1.Text.Replace(' ', '+'));
+            Process.Start("chrome.exe", "https://www.google.rs/search?q=" + "+google+plus+" + textBox1.Text.Replace(' ', '+'));
+            Process.Start("chrome.exe", "https://www.google.rs/search?q=" + "+yelp+" + textBox1.Text.Replace(' ', '+'));
+            Process.Start("chrome.exe", "https://www.google.rs/search?q=" + "+foursquare+" + textBox1.Text.Replace(' ', '+'));
+            Process.Start("chrome.exe", "https://www.google.rs/search?q=" + "+tripadvisor+" + textBox1.Text.Replace(' ', '+'));
+            Process.Start("chrome.exe", "https://www.google.rs/search?q=" + "+timeout+" + textBox1.Text.Replace(' ', '+'));
+            Process.Start("chrome.exe", "https://www.google.rs/search?q=" + "+lonely+planet+" + textBox1.Text.Replace(' ', '+'));
+            Process.Start("chrome.exe", "https://www.google.rs/search?q=" + "+orbitz+" + textBox1.Text.Replace(' ', '+'));
+            Process.Start("chrome.exe", "https://www.google.rs/search?q=" + "+kayak+" + textBox1.Text.Replace(' ', '+'));
+            Process.Start("chrome.exe", "https://www.google.rs/search?q=" + "+oyster.com+" + textBox1.Text.Replace(' ', '+'));
+            Process.Start("chrome.exe", "https://www.google.rs/search?q=" + "+wotif+" + textBox1.Text.Replace(' ', '+'));
+
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
